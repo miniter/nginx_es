f9682a5699bb90c7346a8b90961f613d3a935638
Wayne Brantley <wayne@waynebrantley.com>
2016-08-28 22:24:01 -0400

    support linking with networking and compose

--- startup.sh
+++ startup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 #copy original config over each time docker starts up as the ports/address change!
 /bin/cp -f /etc/nginx/nginx_es.conf.original /etc/nginx/nginx_es.conf
-sed -i "s|<proxy_pass_ip>|$ELASTICSEARCH_PORT_9200_TCP_ADDR|" /etc/nginx/nginx_es.conf
-sed -i "s|<proxy_pass_port>|$ELASTICSEARCH_PORT_9200_TCP_PORT|" /etc/nginx/nginx_es.conf
 
 nginx
